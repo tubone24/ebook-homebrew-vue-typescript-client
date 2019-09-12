8edbec5ff7e3ed161c9661177a0e01929193216c
tubone24 <tubo.yyyuuu@gmail.com>
2019-09-12 23:42:29 +0900

    particle.js

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,25 @@
       <router-link to="/sample">sample</router-link>
     </div>
     <router-view/>
+      <vue-particles
+              color="#add8e6"
+              :particleOpacity="0.7"
+              linesColor="#add8e6"
+              :particlesNumber="80"
+              shapeType="circle"
+              :particleSize="6"
+              :linesWidth="2"
+              :lineLinked="true"
+              :lineOpacity="0.4"
+              :linesDistance="150"
+              :moveSpeed="4"
+              :hoverEffect="true"
+              hoverMode="grab"
+              :clickEffect="true"
+              clickMode="push"
+              retina_detect="true"
+      >
+      </vue-particles>
   </div>
 </template>
 
@@ -14,6 +33,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-size: large;
   color: #2c3e50;
 }
 #nav {
@@ -27,5 +47,16 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#particles-js {
+    background-image: url("./assets/bg.jpg");
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -999;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
       <h1>Ebook Homebrew Home</h1>
+      <br><br>
     <img alt="ebook logo" src="../assets/logo.png">
+      <br><br>
       <h2>How to use it?</h2>
       <a href="https://ebook-homebrew.readthedocs.io/en/latest/">ebook-homebrew Docs</a><br>
       <a href="https://ebook-homebrew.herokuapp.com/docs">ebook-homebrew API Ref</a><br>
